532822b3d1f40c5c3db898b8a16b35f2869230ba
Mark Foster <foster154@gmail.com>
2017-07-10 20:32:49 -0600

    new article

--- src/Components/Posts/_PostData.js
+++ src/Components/Posts/_PostData.js
@@ -1,5 +1,6 @@
 import FirstPost from './2017-04-21-FirstPost'
 import WeekendAway from './2017-05-29-WeekendAway'
+import FirstConference from './2017-07-10-FirstConference'
 
 /*
 {
@@ -12,15 +13,21 @@ import WeekendAway from './2017-05-29-WeekendAway'
 
 export const postData = [
   {
-    title: 'First Post',
-    slug: 'first-post',
-    date: '21 Apr 2017',
-    content: FirstPost
+    title: 'An Introverts Guide to Attending Your First Conference',
+    slug: 'first-conference',
+    date: '10 July 2017',
+    content: FirstConference
   },
   {
     title: 'A Weekend Away',
     slug: 'weekend-away',
     date: '29 May 2017',
     content: WeekendAway
+  },
+  {
+    title: 'First Post',
+    slug: 'first-post',
+    date: '21 Apr 2017',
+    content: FirstPost
   }
 ]
